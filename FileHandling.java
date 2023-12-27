26be276663fb5de9dae647af92554093ca60d820
malik-javed <malikjaved1234000@gmail.com>
2023-12-27 15:20:57 +0530

    File Handling -> CRUD

---
+++ FileHandling.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FileHandling {
+    
+    public static void main(String[] args) throws Exception {
+        /* 
+        
+        // Creating a File :
+        File f = new File("example.txt");
+        f.createNewFile();
+
+
+        // Writing Into the File :
+        FileWriter fw = new FileWriter("example.txt");  //-> Upper File obj f can also be passed
+        fw.write("Generated using the FileWriter ");
+        fw.append("Now using the Append() of FileWriter");
+        fw.close();
+
+
+        // Reading from a File :
+        File f = new File("example.txt");
+        Scanner sc = new Scanner(f);
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
+
+        // Deleting a File
+        File f = new File("example.txt");
+        if(f.delete())
+        {
+            System.out.println("Deleted Succesfully "+ f.getName());
+        }
+    
+       */
+    }
+}
+
