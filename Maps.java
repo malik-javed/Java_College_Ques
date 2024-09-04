104f2f31eaf97911d823f56cd63d77c59043554d
malik-javed <124208510+malik-javed@users.noreply.github.com>
2024-09-05 00:12:27 +0530

    HashMaps

---
+++ Maps.java
@@ -0,0 +1,28 @@
+// import java.util.HashSet;
+// import java.util.Iterator;
+
+// public class Maps {
+//     public static void main(String[] args) {
+//         // HashMap<String , String> hm = new HashMap<>();
+//         HashSet<String> h = new HashSet<>();
+
+        
+        
+        
+//         h.add("Malik");
+//         h.add("malik");
+//         System.out.println(h);
+//         Iterator it = h.iterator();
+        
+//         // System.out.println(it.hasNext());
+
+//         while (it.hasNext()) {
+//             System.out.println(it.next());
+//         }
+        
+
+        
+        
+
+//     }
+// }
